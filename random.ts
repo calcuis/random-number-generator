0d55165b118fe72754859dac1327d6f446d0387b
calcuis <113646141+calcuis@users.noreply.github.com>
2023-07-21 09:42:39 +0800

    Create random.ts

---
+++ random.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function Random() {
+
+  const [minVal, setMinVal] = useState(1);
+  const [maxVal, setMaxVal] = useState(99);
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random()*(maxVal-minVal+1)+minVal));
+  
+  const handlePick = () =>{
+    setRandomNum(Math.floor(Math.random()*(maxVal-minVal+1)+minVal));
+  }
+
+    return (
+        <>
+        <p>Random Number: {randomNum}</p>
+        <input type="number" value={minVal} onChange={e => setMinVal(+e.target.value)}/>
+        <input type="number" value={maxVal} onChange={e => setMaxVal(+e.target.value)}/>
+        <button onClick={handlePick}>Generate</button>
+        </>
+    )
+  }
+  
+  export default Random;
